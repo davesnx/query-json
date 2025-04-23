09e44fa92bb97af01b401b853a5b86683552780a
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-04-23 15:31:34 +0200

    Format js folder

--- js/QueryJsonJs.ml
+++ js/QueryJsonJs.ml
@@ -1,8 +1,7 @@
 let run = QueryJsonCore.run
 
 let () =
-  Js_of_ocaml.Js.export
-    "query-json"
+  Js_of_ocaml.Js.export "query-json"
     (object%js
        val run = Js_of_ocaml.Js.wrap_callback run
     end)
