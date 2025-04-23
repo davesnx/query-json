b58bafaf2ac24a0d14eedb760e375fd1b34695f5
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-04-23 20:55:51 +0200

    Install right deps

--- website/Base64.ml
+++ website/Base64.ml
@@ -1,35 +1,5 @@
-let _ = Webapi.Base64.atob "foo"
-let _ = Webapi.Base64.btoa "gibberish"
-let encode text = Some text
-let decode text = Some text
+let encode text =
+  match Webapi.Base64.btoa text with v -> Some v | exception _ -> None
 
-(* let encode = text => {
-  /* text
-     |> Jstr.v
-     |> Base64.data_utf_8_of_jstr
-     |> Base64.encode
-     |> Result.map(Jstr.to_string)
-     |> Result.to_option; */
-  Some(
-    text,
-  );
-};
-
-let decode = text => {
-  /* switch (text |> Jstr.of_string |> Base64.decode) {
-     | Ok(dec) =>
-       switch (Base64.data_utf_8_to_jstr(dec)) {
-       | Ok(str) => Some(Jstr.to_string(str))
-       | Error(e) =>
-         Printf.eprintf("decode 1 %s", e |> error_to_string);
-         None;
-       }
-     | Error(e) =>
-       Printf.eprintf("decode 2 %s", e |> error_to_string);
-       None;
-     }; */
-  Some(
-    text,
-  );
-};
- *)
+let decode text =
+  match Webapi.Base64.atob text with v -> Some v | exception _ -> None
