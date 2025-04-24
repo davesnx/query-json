86864a4c3e6551cfa6d6a1317cef71767078400e
David Sancho Moreno <dsnxmoreno@gmail.com>
2025-04-24 10:42:29 +0200

    Fix tailwind

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,4 +1,4 @@
 export default {
-  content: ["./**/*.mlx", "./index.html", "../_build/default/website/website/website/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "*.mlx", "./website/**/*.{js,ts,jsx,tsx}"],
   plugins: {}
 }
