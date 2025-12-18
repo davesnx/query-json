ce6c9b4a48f106eba4420f26247d088044f7703b
David Sancho Moreno <david.sancho@ahrefs.com>
2025-12-18 11:54:33 +0000

    Remove eazy_format

--- bin/bin.ml
+++ bin/bin.ml
@@ -5,85 +5,66 @@ module Info = struct
     | Some v -> Build_info.V1.Version.to_string v
 
   let description =
-    "query-json allows to write small programs to operate on top of JSON files \
-     with a concise syntax. It's a faster and simpler re-implementation of jq \
-     in OCaml"
+    "query-json allows you to write small programs to operate on top of json \
+     files with a concise syntax. It's a faster, simpler and more portable \
+     re-implementation of jq in OCaml"
 end
 
 let print_error_message ~colorize str =
-  let module Chalk = Chalk.Make (struct
-    let disable = not colorize
+  let open Formatting in
+  let open Ansi.To_string (struct
+    let colorize = colorize
   end) in
   print_endline
-    (Formatting.enter 1
-    ^ Chalk.red (Chalk.bold "Error")
-    ^ Chalk.red ":" ^ Formatting.indent 1 ^ str ^ Formatting.enter 1)
+    (enter 1 ^ red (bold "Error") ^ red ":" ^ indent 1 ^ str ^ enter 1)
 
 let usage ?(colorize = true) () =
   let open Formatting in
-  let module Chalk = Chalk.Make (struct
-    let disable = not colorize
+  let open Ansi.To_string (struct
+    let colorize = colorize
   end) in
   [
     enter 1;
-    Chalk.yellow "Missing query as argument";
-    enter 1 ^ "Usage:" ^ enter 2 ^ Chalk.bold "query-json"
-    ^ Chalk.gray " [OPTIONS] " ^ "[QUERY] [JSON]" ^ enter 2
-    ^ Chalk.bold "OPTIONS";
+    yellow "Missing query as argument";
+    enter 1 ^ "Usage:" ^ enter 2 ^ bold "query-json" ^ gray " [OPTIONS] "
+    ^ "[QUERY] [JSON]" ^ enter 2 ^ bold "OPTIONS";
     indent 1 ^ "-c, --no-color: Disable color in the output";
     indent 1 ^ "-r, --raw-output: Output raw strings, not JSON texts";
     indent 1 ^ "-v, --verbose: Activate verbossity";
     indent 1 ^ "-d, --debug: Print AST";
     indent 1 ^ "--version: Show version information." ^ enter 2
-    ^ Chalk.bold "EXAMPLES";
+    ^ bold "EXAMPLES";
     indent 1 ^ "query-json '.dependencies' package.json";
-    indent 1 ^ "query-json '.' <<< '[1, 2, 3]'" ^ enter 2 ^ Chalk.bold "MORE";
+    indent 1 ^ "query-json '.' <<< '[1, 2, 3]'" ^ enter 2 ^ bold "MORE";
     indent 1 ^ " https://github.com/davesnx/query-json";
     enter 1;
   ]
   |> String.concat (enter 1)
   |> print_endline
 
-module Runtime = struct
-  let run ~payload ~no_color ~verbose ~raw_output runtime =
-    let colorize = not no_color in
-    let input =
-      match payload with
-      | Some file_or_json ->
-          if Sys.file_exists file_or_json then Json.parse_file file_or_json
-          else Json.parse_string file_or_json
-      | None ->
-          let ic = Unix.in_channel_of_descr Unix.stdin in
-          Json.parse_channel ic
-    in
-    match input with
-    | Ok json -> (
-        match runtime ~colorize ~verbose json with
-        | Ok json ->
-            json
-            |> List.map
-                 (Json.to_string ~colorize ~summarize:false ~raw:raw_output)
-            |> List.iter print_endline
-        | Error err -> print_error_message ~colorize err)
-    | Error err -> print_error_message ~colorize err
-end
+let ( let* ) = Result.bind
 
-let execution (query : string option) (payload : string option) (verbose : bool)
-    (debug : bool) (no_color : bool) (raw_output : bool) =
+let execution query payload verbose debug no_color raw_output =
   let colorize = not no_color in
   match query with
-  | Some query -> (
-      let runtime =
-        Core.parse ~debug ~colorize ~verbose query
-        |> Result.map (fun expr ->
-            fun ~colorize ~verbose json ->
-             Interpreter.execute ~colorize ~verbose expr json)
-      in
-      match runtime with
-      | Ok runtime ->
-          Runtime.run ~payload ~no_color ~verbose ~raw_output runtime
-      | Error err -> print_error_message ~colorize err)
   | None -> usage ()
+  | Some query -> (
+      let output =
+        let* expr = Core.parse ~debug ~colorize ~verbose query in
+        let* json =
+          match payload with
+          | Some f when Sys.file_exists f -> Json.parse_file f
+          | Some s -> Json.parse_string s
+          | None -> Json.parse_channel (Unix.in_channel_of_descr Unix.stdin)
+        in
+        Interpreter.execute ~colorize ~verbose expr json
+      in
+      match output with
+      | Ok results ->
+          List.iter
+            (Json.print ~colorize ~summarize:false ~raw:raw_output)
+            results
+      | Error err -> print_error_message ~colorize err)
 
 let () =
   let open Cmdliner.Arg in
--- source/Core.ml
+++ source/Core.ml
@@ -1,5 +1,3 @@
-open Base
-
 module Location = struct
   type t = { loc_start : Lexing.position; loc_end : Lexing.position }
 
@@ -16,7 +14,7 @@ let provider ~debug buf =
     match Lexer.tokenize buf with Ok t -> t | Error e -> raise (Lexer_error e)
   in
   last_position := { loc_start = start; loc_end = stop };
-  if debug then Stdio.print_endline (Lexer.show_token token);
+  if debug then print_endline (Lexer.show_token token);
   (token, start, stop)
 
 let menhir = MenhirLib.Convert.Simplified.traditional2revised Parser.program
@@ -28,20 +26,20 @@ let position_to_string start end_ =
 
 let pretty_print_error ~colorize ~input ~(start : Lexing.position)
     ~(end_ : Lexing.position) =
-  let module Chalk = Chalk.Make (struct
-    let disable = not colorize
+  let module Color = Ansi.To_string (struct
+    let colorize = colorize
   end) in
   let pointer_range = String.make (end_.pos_cnum - start.pos_cnum) '^' in
-  Chalk.red (Chalk.bold "Parse error: ")
+  Color.red (Color.bold "Parse error: ")
   ^ "Problem parsing at position "
   ^ position_to_string start end_
   ^ Formatting.enter 2 ^ "Input:" ^ Formatting.indent 1
-  ^ Chalk.green (Chalk.bold input)
+  ^ Color.green (Color.bold input)
   ^ Formatting.enter 1 ^ Formatting.indent 4
   ^ String.make start.pos_cnum ' '
-  ^ Chalk.gray pointer_range
+  ^ Color.gray pointer_range
 
-let parse ?(debug = false) ?(colorize = true) ?(verbose : _) input :
+let parse ?(debug = false) ?(colorize = true) ?(verbose = false) input :
     (Ast.expression, string) result =
   let _ = ignore verbose in
   (* verbose will be used for parser warnings in the future *)
@@ -49,13 +47,13 @@ let parse ?(debug = false) ?(colorize = true) ?(verbose : _) input :
   let next_token () = provider ~debug buf in
   match menhir next_token with
   | ast ->
-      if debug then Stdio.print_endline (Ast.show_expression ast);
+      if debug then print_endline (Ast.show_expression ast);
       Ok ast
   | exception Lexer_error msg ->
       (* TODO: Do we want to show the lexing error differently than the parser error? *)
       if debug then (
-        Stdio.print_endline "Lexer error";
-        Stdio.print_endline msg);
+        print_endline "Lexer error";
+        print_endline msg);
       let Location.{ loc_start; loc_end; _ } = !last_position in
       Error (pretty_print_error ~colorize ~input ~start:loc_start ~end_:loc_end)
   | exception _exn ->
@@ -65,12 +63,12 @@ let parse ?(debug = false) ?(colorize = true) ?(verbose : _) input :
 let run query json =
   match parse ~debug:false ~colorize:false ~verbose:false query with
   | Ok runtime ->
-      let ( let* ) x f = Result.bind x ~f in
+      let ( let* ) = Result.bind in
       let* results =
         Interpreter.execute ~colorize:false ~verbose:false runtime json
       in
       Ok
         (results
-        |> List.map ~f:(Json.to_string ~colorize:false ~summarize:false ~raw:false)
-        |> String.concat ~sep:"\n")
+        |> List.map (Json.to_string ~colorize:false ~summarize:false ~raw:false)
+        |> String.concat "\n")
   | Error err -> Error err
